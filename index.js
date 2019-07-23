ca5d408d31971689a8cec4e5b00cb6090add20f3
ruizer <liaorui0610@gmail.com>
2019-07-23 20:22:04 +0800

    解决 iframe 加载完成之前调用 window.print()的bug

--- index.js
+++ index.js
@@ -11,65 +11,73 @@ class Print {
 
   /** @description 特定的打印样式 */
   printStyle() {
-    let mode = this.mode.toLowerCase()
-    mode = mode === 'landscape' ? mode : 'portrait'
-    const style = document.createElement('style')
-    style.type = 'text/css'
-    style.innerHTML = `@page { size: ${mode}; margin: 10mm;}`
-    return style
+    let mode = this.mode.toLowerCase();
+    mode = mode === 'landscape' ? mode : 'portrait';
+    const style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = `@page { size: ${mode}; margin: 10mm;}`;
+    return style;
   }
 
   /**
    * @description 获取当前页面所有的style、link、meta及传入的dom对象
    * */
   printAllDom() {
-    let copyDom = document.createElement('span')
-    const styleDom = document.querySelectorAll('style, link, meta')
-    const titleDom = document.createElement('title')
-    titleDom.innerText = this.title
-    copyDom.appendChild(titleDom)
+    let copyDom = document.createElement('span');
+    const styleDom = document.querySelectorAll('style, link, meta');
+    const titleDom = document.createElement('title');
+    titleDom.innerText = this.title;
+    copyDom.appendChild(titleDom);
     Array.from(styleDom).forEach(item => {
-        copyDom.appendChild(item.cloneNode(true))
-    })
-    copyDom.appendChild(this.printStyle())
-    copyDom.appendChild(this.dom.cloneNode(true))
-    this.htmlTemp = copyDom.innerHTML
-    copyDom = null
+      copyDom.appendChild(item.cloneNode(true));
+    });
+    copyDom.appendChild(this.printStyle());
+    this.headTemp = copyDom.innerHTML;
+    copyDom = null;
   }
 
   /**
    * @description 生成iframe
    */
   printIframeDom() {
-    const iframeDom = document.createElement('iframe')
+    const iframeDom = document.createElement('iframe');
     const attrObj = {
-        height: 0,
-        width: 0,
-        border: 0,
-        wmode: 'Opaque'
-    }
+      height: 0,
+      width: 0,
+      border: 0,
+      wmode: 'Opaque'
+    };
     const styleObj = {
-        position: 'absolute',
-        top: '-999px',
-        left: '-999px'
-    }
-    Object.entries(attrObj).forEach(([key, value]) => { iframeDom.setAttribute(key, value) })
-    Object.entries(styleObj).forEach(([key, value]) => { iframeDom.style[key] = value })
-    document.body.insertBefore(iframeDom, document.body.children[0])
-    const iframeWin = iframeDom.contentWindow
-    const iframeDocs = iframeWin.document
-    iframeDocs.write(`<!doctype html>`)
-    iframeDocs.write(this.htmlTemp)
-    iframeDocs.body.setAttribute('style', '-webkit-print-color-adjust: exact;')
-    iframeWin.focus()
-    iframeWin.print()
-    document.body.removeChild(iframeDom)
+      position: 'absolute',
+      top: '-999px',
+      left: '-999px'
+    };
+    Object.entries(attrObj).forEach(([key, value]) => {
+      iframeDom.setAttribute(key, value);
+    });
+    Object.entries(styleObj).forEach(([key, value]) => {
+      iframeDom.style[key] = value;
+    });
+    iframeDom.srcdoc = `<html><head>${
+      this.headTemp
+    }</head></head><body style="-webkit-print-color-adjust: exact;">${
+      this.dom.outerHTML
+    }</body></html>`;
+    this.iframeDom = iframeDom;
   }
 
   /** @description 开始打印*/
   start() {
-    this.printAllDom()
-    this.printIframeDom()
+    this.printAllDom();
+    this.printIframeDom();
+    document.body.insertBefore(this.iframeDom, document.body.children[0]);
+    const iframeWin = this.iframeDom.contentWindow;
+    this.iframeDom.onload = () => {
+      iframeWin.focus();
+      iframeWin.print();
+      document.body.removeChild(this.iframeDom);
+      this.iframeDom = null;
+    };
   }
 }
 
@@ -77,4 +85,4 @@ if (typeof window !== 'undefined') {
   window.printjs = Print;
 }
 
-export default Print
+export default Print;
